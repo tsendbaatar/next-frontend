15178ca13a040b58ea2aa2c1ee304f6d70182ef3
TsendbaatarO <tsendbaatar.o@gmail.com>
2024-03-29 14:55:54 +0800

    20240329

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/application.scss'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import { Accordion } from 'react-bootstrap'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>NextJS app with react-bootstrap</title>
+      </Head>
+
+      <main>
+        <h1>
+          Welcome
+        </h1>
+
+        <Accordion defaultActiveKey="0">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Accordion Item #1</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet...
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Accordion Item #2</Accordion.Header>
+            <Accordion.Body>
+              Duis aute irure dolor in reprehenderit in voluptate...
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+
+      </main>
+    </div>
+  )
+}
